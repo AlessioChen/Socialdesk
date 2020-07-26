7221f1ff3877ca0706a16fb8cd1300dda3c0280f
alessio <alessiochen98@gmail.com>
2020-07-26 16:57:27 +0200

    bootstap for nav bar

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+from django.http import HttpResponse
 # Create your views here.
 
-def home(request): 
-    return HttpResponse('<h1> Blog Home</h1>')
+posts  =[
+    {
+        'author': 'Alessio', 
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'today'
+    },
+
+     {
+        'author': 'Luca', 
+        'title': 'Blog Post 2',
+        'content': 'Seconde post content',
+        'date_posted': 'yesterday'
+    },
+]
+
+def home(request):
+    context = {
+        'posts': posts, 
+        'title': 'ciccio bello'
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return HttpResponse('<h1> Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})

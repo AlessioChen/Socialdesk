4d954331752030655833fba55a5b0822f604b6bf
alessio <alessiochen98@gmail.com>
2020-07-28 20:51:23 +0200

    Created the model for Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.db import models  #models Master class
+from django.utils import timezone
+from django.contrib.auth.models import User #default Django User table
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    post_date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # if a user is deleted also the post is deleted
+    

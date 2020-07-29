4fd1d18f1efc8025bbc489389c7e6cf4fb31f6d2
alessio <alessiochen98@gmail.com>
2020-07-29 16:59:43 +0200

    added crispy app for forms

--- Socialdesk/settings.py
+++ Socialdesk/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     #my own app 
     'blog',
     'users',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'

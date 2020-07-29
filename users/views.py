0e1d1c93b39fb5181c949d191fa6477d688dd426
alessio <alessiochen98@gmail.com>
2020-07-29 11:13:50 +0200

    messages and usercreation

--- users/views.py
+++ users/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def register(request):
-    form = UserCreationForm()
-    context = {
-        'form': form
-    }
+    #GET we disply the form 
+    #POST we save the data
 
+    context = { }
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid(): 
+            username  = form.cleaned_data.get("username")
+            messages.success(request, f"Account created for {username}!")
+
+            return redirect('blog-home') #redirect to the home page if success
+            
+    else: 
+        form = UserCreationForm()
+        context = {
+            'form': form
+        }
     
     return render(request, 'users/register.html', context)

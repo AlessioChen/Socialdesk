bdeb1d02a953ef95c5e7400259b2dd30b29727df
alessio <alessiochen98@gmail.com>
2020-07-29 11:29:28 +0200

    new registration form with email field

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 def register(request):
     #GET we disply the form 
@@ -9,17 +10,17 @@ def register(request):
     context = { }
 
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid(): 
             username  = form.cleaned_data.get("username")
-            messages.success(request, f"Account created for {username}!")
-
+            messages.success(request, f"Account created for {username}!") #flash message
+            form.save() #saves the data in the DB
             return redirect('blog-home') #redirect to the home page if success
             
     else: 
-        form = UserCreationForm()
-        context = {
-            'form': form
-        }
-    
+        form = UserRegisterForm()
+      
+    context = {
+        'form': form
+    }
     return render(request, 'users/register.html', context)

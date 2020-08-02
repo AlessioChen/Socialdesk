10c006e7f0fc1079772a534c6df9a293719d35e6
alessio <alessiochen98@gmail.com>
2020-08-02 11:19:09 +0200

    profile page with login require

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     #GET we disply the form 
@@ -24,3 +25,7 @@ def register(request):
         'form': form
     }
     return render(request, 'users/register.html', context)
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
